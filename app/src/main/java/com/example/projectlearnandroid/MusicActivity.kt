a22dc2497f2b0dd8c65ac18712533846847eb0f0
Phan Vuong <vuong.phan@monstar-lab.com>
2022-11-12 17:18:51 +0700

    test ui

--- app/src/main/java/com/example/projectlearnandroid/MusicActivity.kt
+++ app/src/main/java/com/example/projectlearnandroid/MusicActivity.kt
@@ -1,11 +1,18 @@
 package com.example.projectlearnandroid
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_music.*
 
 class MusicActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_music)
+
+        lg.setOnClickListener {
+            val intent = Intent(this, MusicDetailActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
